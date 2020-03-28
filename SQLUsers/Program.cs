c21d57e2d3b76015294398168fbc7928998a08a1
A.Benlemonade <adambenlemlih24@gmail.com>
2020-03-28 12:05:31 +0100

    Add project files.

---
+++ SQLUsers/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLUsers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool finished = false;
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("The number of users: " + UserService.getNumberOfUsers());
+                if (!AuthenticationService.isAuthenticated())
+                {
+                    User authernticatedUser = AuthenticationService.getAuthenticatedUser();
+                    Console.WriteLine("1. Login");
+                    Console.WriteLine("2. Register");
+                    Console.WriteLine("3. Quit");
+                    string choice = Console.ReadLine();
+                    switch (choice)
+                    {
+                        case "1":
+                            Console.Write("Username: ");
+                            string LoginUsername = Console.ReadLine();
+                            Console.Write("Password: ");
+                            string LoginPassword = Console.ReadLine();
+                            if (AuthenticationService.authenticate(LoginUsername, LoginPassword))
+                            {
+                                Console.WriteLine("Login Success! ");
+                            }
+                            else
+                            {
+                                Console.WriteLine("The username or password was incorrect or not registered.");
+                            }
+                            break;
+                        case "2":
+                            Console.Write("Username: ");
+                            string username = Console.ReadLine();
+                            Console.Write("Password: ");
+                            string password = Console.ReadLine();
+                            UserService.registerNewUser(username, password);
+                            break;
+                        case "3":
+                            finished = true;
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    User authenticatedUser = AuthenticationService.getAuthenticatedUser();
+                    Console.WriteLine("Hello, " + authenticatedUser.GetUsername() + "!");
+                    Console.WriteLine("1. List Data");
+                    Console.WriteLine("2. Record New Data");
+                    Console.WriteLine("3. Log Out");
+                    Console.WriteLine("4. Quit");
+                    string choice = Console.ReadLine();
+                    switch (choice)
+                    {
+                        case "1":
+                            Dictionary<string, string> data = UserDataService.getAllDataForUser(AuthenticationService.getAuthenticatedUser().GetID());
+                            foreach (KeyValuePair<string, string> item in data)
+                            {
+                                Console.WriteLine(item.Key + ": " + item.Value);
+                            }
+                            Console.ReadKey();
+                            break;
+                        case "2":
+                            Console.WriteLine("Data Type: ");
+                            string dataKey = Console.ReadLine();
+                            Console.WriteLine("Data Value: ");
+                            string dataValue = Console.ReadLine();
+                            UserDataService.storeUserData(AuthenticationService.getAuthenticatedUser().GetID(), dataKey, dataValue);
+                            break;
+                        case "3":
+                            AuthenticationService.logout();
+                            Console.WriteLine("Logged out. Adios muchacho.");
+                            break;
+                        case "4":
+                            finished = true;
+                            break;
+                    
+                    }
+                }
+            } while (!finished);
+        }
+    }
+}
---
+++ SQLUsers/User.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SQLUsers
+{
+    class User
+    {
+        private int id;
+        private string username;
+        private string password;
+        public User(int id, string username, string password)
+        {
+            this.id = id;
+            this.username = username;
+            this.password = password;
+        }
+
+        public int GetID()
+        {
+            return id;  
+        }
+
+        public string GetUsername()
+        {
+            return username;
+        }
+
+        public string GetPassword()
+        {
+            return password;
+        }
+    }
+}
